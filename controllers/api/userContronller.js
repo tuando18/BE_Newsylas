ea830e1b12e45e8c6b80d8b7368d552e5e5d9061
tuando18 <tuaansdo.18@gmail.com>
2024-11-07 20:51:55 +0700

    update

--- controllers/api/userContronller.js
+++ controllers/api/userContronller.js
@@ -1,45 +1,55 @@
-const User = require("../../models/userModel");
-const jwt = require("jsonwebtoken");
 const UserService = require("../../services/userService");
 const HttpResponse = require("../../utils/httpResponse");
 
 class UserController {
   login = async (req, res) => {
     const { username, password } = req.body;
-
     try {
-        const data = await new UserService().login(username, password);
-
-        if (data && data.status === 200) {     
-            console.log(data);
-            req.session.admin = data;
-            return res.json(HttpResponse.resultAuth(data));
-        } else {
-            return res.json(HttpResponse.fail(HttpResponse.getErrorMessages('loginFail')));
-        }
+      const data = await new UserService().login(username, password);
+      if (data && data.status === 200) {     
+          req.session.admin = data;
+          return res.json(HttpResponse.resultAuth(data));
+      } else {
+          return res.json(HttpResponse.fail(HttpResponse.getErrorMessages('loginFail')));
+      }
     } catch (error) {
-        console.log(error);
-        return res.json(HttpResponse.error(error));
+      console.log(error);
+      return res.json(HttpResponse.error(error));
     }
   };
 
   register = async (req, res) => {
     const { username, password } = req.body;
+    try {
+      const data = await new UserService().register(username, password);
+      if (data && data.status === 200) {
+          return res.json(HttpResponse.resultAuth(data));
+      } else {
+          return res.json(HttpResponse.fail(HttpResponse.getErrorMessages('registerFail')));
+      }
+    } catch (error) {
+      console.log(error);
+      return res.json(HttpResponse.error(error));
+    }
+  };
+
+  // Thêm endpoint để lưu thông tin ví Solana
+  saveWallet = async (req, res) => {
+    const { publicKey, solanaTokenBalance } = req.body;
+    const userId = req.session.admin.id;
 
     try {
-        const data = await new UserService().register(username, password);
-
-        if (data && data.status === 200) {
-            return res.json(HttpResponse.resultAuth(data));
-        } else {
-            return res.json(HttpResponse.fail(HttpResponse.getErrorMessages('registerFail')));
-        }
+      const data = await new UserService().updateWalletInfo(userId, publicKey, solanaTokenBalance);
+      if (data && data.status === 200) {
+          return res.json(HttpResponse.success({ message: 'Wallet info saved successfully' }));
+      } else {
+          return res.json(HttpResponse.fail({ message: 'Failed to save wallet info' }));
+      }
     } catch (error) {
-        console.log(error);
-        return res.json(HttpResponse.error(error));
+      console.log(error);
+      return res.json(HttpResponse.error(error));
     }
   };
 }
 
 module.exports = UserController;
-
--- services/userService.js
+++ services/userService.js
@@ -1,11 +1,9 @@
 const Users = require("../models/userModel");
 const JWT = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const HttpResponse = require("../utils/httpResponse");
-const UtilsFunctions = require("../utils/utilsFunction");
 const dotenv = require('dotenv');
 dotenv.config();
-const SECRETKEY = process.env.SECRETKEY
+const SECRETKEY = process.env.SECRETKEY;
 
 class UserService {
     register = async (username, password, role = 'user') => {
@@ -14,28 +12,25 @@ class UserService {
             if (existingUser) {
                 return HttpResponse.fail({ message: 'Username already exists' });
             }
-    
-            // Lưu mật khẩu dạng văn bản thuần túy
+
             const newUser = await Users.create({ username, password, role });
-    
             const token = JWT.sign({ id: newUser._id, role: newUser.role }, SECRETKEY, { expiresIn: '1h' });
             const refreshToken = JWT.sign({ id: newUser._id, role: newUser.role }, SECRETKEY, { expiresIn: '1d' });
-    
+
             return HttpResponse.auth(newUser, token, refreshToken);
         } catch (error) {
             console.log(error);
             return HttpResponse.error(error);
         }
     };
-    
+
     login = async (username, password) => {
         try {
-            // Tìm người dùng và kiểm tra trực tiếp mật khẩu thô
             const user = await Users.findOne({ username, password });
             if (user) {
                 const token = JWT.sign({ id: user._id, role: user.role }, SECRETKEY, { expiresIn: '1h' });
                 const refreshToken = JWT.sign({ id: user._id }, SECRETKEY, { expiresIn: '1d' });
-    
+
                 return HttpResponse.auth(user, token, refreshToken);
             } else {
                 return HttpResponse.fail(HttpResponse.getErrorMessages('loginFail'));
@@ -45,7 +40,25 @@ class UserService {
             return HttpResponse.error(error);
         }
     };
-    
+
+    // Thêm phương thức updateWalletInfo để lưu publicKey và solanaTokenBalance
+    updateWalletInfo = async (userId, publicKey, solanaTokenBalance) => {
+        try {
+            const updatedUser = await Users.findByIdAndUpdate(
+                userId,
+                { publicKey, solanaTokenBalance },
+                { new: true }  // Trả về tài liệu đã cập nhật
+            );
+            if (updatedUser) {
+                return HttpResponse.success(updatedUser);
+            } else {
+                return HttpResponse.fail({ message: 'User not found' });
+            }
+        } catch (error) {
+            console.log(error);
+            return HttpResponse.error(error);
+        }
+    };
 }
 
 module.exports = UserService;
